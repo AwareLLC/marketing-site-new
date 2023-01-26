4558270e765653a3d942d8c7cac83a0c0063ff1a
Mac Martine <mac.martine@gmail.com>
2023-01-26 12:27:04 +0100

    Temp msg

--- src/components/PrimaryFeatures.jsx
+++ src/components/PrimaryFeatures.jsx
@@ -72,11 +72,10 @@ export function PrimaryFeatures() {
       <Container className="relative">
         <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
-            Everything you need to run your books.
+            Everything you need to grow your to crush your social selling goals.
           </h2>
           <p className="mt-6 text-lg tracking-tight text-blue-100">
-            Well everything you need if you aren’t that picky about minor
-            details like tax compliance.
+            Everything you need to grow your personal brand.
           </p>
         </div>
         <Tab.Group
