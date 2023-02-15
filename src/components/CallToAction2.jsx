3410eb84edffebd6cd9559636727665c7328ba00
Mac Martine <mac.martine@gmail.com>
2023-02-15 11:34:30 +0100

    Escape

--- src/components/CallToAction2.jsx
+++ src/components/CallToAction2.jsx
@@ -21,13 +21,13 @@ export function CallToAction2() {
       <Container className="relative">
         <div className="mx-auto max-w-lg text-center">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
-            You're still reading? What are you waiting for?
+            You&apos;re still reading? What are you waiting for?
           </h2>
           <p className="mt-4 text-lg tracking-tight text-white">
-            Start your 7-day free trial. You've got nothing to lose, plus, you won't regret it.
+            Start your 7-day free trial. You&apos;ve got nothing to lose, plus, you won&apos;t regret it.
           </p>
           <Button href="https://app.useaware.co" color="white" className="mt-10">
-            Let's go!
+            Let&apos;s go!
           </Button>
         </div>
       </Container>
